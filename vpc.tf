ac8f595f915e4e50bdd67123f117a0e12931e53d
Anilgollena <115261722+Anilgollena@users.noreply.github.com>
2024-10-21 16:23:36 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+#creating vpc
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "demovpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo VPC"
+  }
+}
