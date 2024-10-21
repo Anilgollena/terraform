5bf81a92fc8b195eea0d16af900a61729d2ede83
Anilgollena <115261722+Anilgollena@users.noreply.github.com>
2024-10-21 16:54:55 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -6,6 +6,6 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone 
+git clone https://github.com/Anilgollena/docker.git
 cd wordpress/
 docker-compose up -d
